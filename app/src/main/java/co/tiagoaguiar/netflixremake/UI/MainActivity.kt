01e798ebfac76042bf4c5bde2bfe5787f1422bda
JULIANO RENIS GOMES SILVA DOS SANTOS <julianorenis@gmail.com>
2022-12-30 12:01:57 -0300

    -Estruturar o esqueleto da recycleView adicionando dados falsos para testes. - Organizando pastas e arquivos

--- app/src/main/java/co/tiagoaguiar/netflixremake/UI/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/netflixremake/UI/MainActivity.kt
@@ -1,18 +1,26 @@
-package co.tiagoaguiar.netflixremake
+package co.tiagoaguiar.netflixremake.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import co.tiagoaguiar.netflixremake.R
+import co.tiagoaguiar.netflixremake.model.Movie
 
 class MainActivity : AppCompatActivity() {
+              /* Model View/Controller - MVC */
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val adapter = MainAdapter()
+        val movies = mutableListOf<Movie>()
+        for (i in 0 until 60) {
+            val movie = Movie("http://exemplo.com/$i.jpg")
+            movies.add(movie)
+        }
+        val adapter = MainAdapter(movies)
         val rv: RecyclerView = findViewById(R.id.rv_main)
         rv.layoutManager = LinearLayoutManager(this)
         rv.adapter = adapter
---
+++ app/src/main/java/co/tiagoaguiar/netflixremake/UI/MainAdapter.kt
@@ -0,0 +1,36 @@
+package co.tiagoaguiar.netflixremake.UI
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import co.tiagoaguiar.netflixremake.R
+import co.tiagoaguiar.netflixremake.model.Movie
+
+class MainAdapter (private val movies:List<Movie>): RecyclerView.Adapter<MainAdapter.MovieViewHolder>(){
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.movie_item,parent,false)
+        return MovieViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        val movie = movies[position]
+        holder.bind(movie)
+
+    }
+
+    override fun getItemCount(): Int {
+        return movies.size
+    }
+
+     inner class  MovieViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+         fun bind(movie: Movie) {
+            val textFilm: TextView = itemView.findViewById(R.id.txt_id_text)
+             textFilm.text = movie.coverUrl
+         }
+     }
+
+}

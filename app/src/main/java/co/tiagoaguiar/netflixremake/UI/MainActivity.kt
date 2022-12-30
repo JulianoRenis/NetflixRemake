3e72d3eae85d5507a5951431abbe469d77b9718c
JULIANO RENIS GOMES SILVA DOS SANTOS <julianorenis@gmail.com>
2022-12-30 18:01:41 -0300

    Netiflixapp :UI Utilizando sua própria barra de ação: Toolbar
    -Netiflix Remake : iniciamos a toolbar na MovieActivity(porém acredito que é possível criar uma toolbar fora do layout da main e reutilizar em outras activities) * reaproveitar o código*
    
    - Usar supportActionBar?.setHomeAsUpIndicator para chamar umaimgaem(botão voltar).
    - setDisplayHomeAsUpEnable true para efetivamente mostrar a imagem do botão voltar.

--- app/src/main/java/co/tiagoaguiar/netflixremake/UI/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/netflixremake/UI/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import co.tiagoaguiar.netflixremake.R
+import co.tiagoaguiar.netflixremake.adapter.CategoryAdapter
+import co.tiagoaguiar.netflixremake.model.Category
 import co.tiagoaguiar.netflixremake.model.Movie
 
 class MainActivity : AppCompatActivity() {
@@ -15,14 +17,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val movies = mutableListOf<Movie>()
-        for (i in 0 until 60) {
-            val movie = Movie(R.drawable.movie)
-            movies.add(movie)
+        //
+
+        val categories = mutableListOf<Category>()
+        for(j in 0 until 10){
+            val movies = mutableListOf<Movie>()
+
+            for (i in 0 until 15) {
+                val movie = Movie(R.drawable.movie)
+                movies.add(movie)
+            }
+            val category = Category("cat $j", movies )
+            categories.add(category)
         }
-        val adapter = MainAdapter(movies)
+
+        val adapter = CategoryAdapter(categories)
         val rv: RecyclerView = findViewById(R.id.rv_main)
-        rv.layoutManager = LinearLayoutManager(this,RecyclerView.HORIZONTAL,false)
+        rv.layoutManager = LinearLayoutManager(this)
         rv.adapter = adapter
 
     }

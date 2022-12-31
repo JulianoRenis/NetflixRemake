813973522a55f1b5997079113eb0d994d92a2efb
JULIANO RENIS GOMES SILVA DOS SANTOS <julianorenis@gmail.com>
2022-12-31 15:21:00 -0300

    Netflix App : Recursos Online e API -> Consumindo dados remotos: Leitura Byte a Byte(parte2) - Comunicação Http criada - Dados sendo lançados na MainActivity.kt - Http puro /* nos próximos módulos usaremos frameworks recomendados pelo google */

--- app/src/main/java/co/tiagoaguiar/netflixremake/UI/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/netflixremake/UI/MainActivity.kt
@@ -1,6 +1,10 @@
 package co.tiagoaguiar.netflixremake.UI
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -9,22 +13,43 @@ import co.tiagoaguiar.netflixremake.adapter.CategoryAdapter
 import co.tiagoaguiar.netflixremake.model.Category
 import co.tiagoaguiar.netflixremake.util.CategoryTask
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CategoryTask.Callback {
     /* Model View/Controller - MVC */
 
+    private lateinit var progres: ProgressBar
+    private lateinit var adapter: CategoryAdapter
+    private val categories = mutableListOf<Category>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val categories = mutableListOf<Category>()
-
-        val adapter = CategoryAdapter(categories)
+        progres = findViewById(R.id.progress_main)
+         adapter = CategoryAdapter(categories)
         val rv: RecyclerView = findViewById(R.id.rv_main)
         rv.layoutManager = LinearLayoutManager(this)
         rv.adapter = adapter
 
-        CategoryTask().excute("https://api.tiagoaguiar.co/netflixapp/home?apiKey=379fa363-4924-4e3c-8697-6150e553388a")
+        CategoryTask(this).excute("https://api.tiagoaguiar.co/netflixapp/home?apiKey=379fa363-4924-4e3c-8697-6150e553388a")
+    }
+
+    override fun onResult(categories: List<Category>) {
+        Log.i("Teste main Activity", categories.toString())
+        this.categories.clear()
+        this.categories.addAll(categories)
+        adapter.notifyDataSetChanged()//  força o adapter chamar o onBindViewHolder
+        progres.visibility = View.GONE
+
+    }
+
+    override fun onFailure(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        progres.visibility = View.GONE
+
+    }
+
+    override fun onPreExecute() {
+        progres.visibility = View.VISIBLE
     }
 
 }
